a66f1c59a8ef17d8d73ba51e201a47e856e6f7c8
Shankar Rajan <136696617+shankar-ranjan@users.noreply.github.com>
2023-10-04 10:53:47 +0530

    Dashboard code added

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,43 @@ function App() {
   return (
     <div className="App">
       <p>Welcome to chrome extension</p>
+      <Card >
+                        <Row>
+                            <Col>
+                                <Image
+                                    width={50}
+                                    src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                                />
+                            </Col>
+                            <Col className="img-left-text">
+                                <h5 className="neeraj">Neeraj Sharma</h5>
+                                <h5 className="project">Project Manger</h5>
+                            </Col>
+                            <Col>
+                                <DashOutlined />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <h6>PHONE NO:</h6>
+                                <h6>+91 9993335550</h6>
+                            </Col>
+                            <Col>
+                                <h6>CONTACT TYPE</h6>
+                                <h6>Individual</h6>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <h6>EMAIL</h6>
+                                <h6>neeraj@cyclops.com</h6>
+                            </Col>
+                            <Col>
+                                <h6>ROLE</h6>
+                                <h6>Employee</h6><CaretDownOutlined />
+                            </Col>
+                        </Row>
+                    </Card>
     </div>
   );
 }
